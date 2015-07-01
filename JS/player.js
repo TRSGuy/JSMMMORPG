eca78c9d09cf25a6b955fc08bffc52de176b1db4
TRSGuy <kimpankarl@gmail.com>
2015-07-01 22:22:10 +0200

    Update player.js

--- JS/player.js
+++ JS/player.js
@@ -2,21 +2,30 @@ var player = {
 	x: 0,
 	y: 0,
 	speed: 0,
-	create: function(x , y, speed) {
-		var obj = Object.create(this);
-		obj.x = x;
-		document.onkeydown = this.keyDown;
-		obj.y = y;
-		obj.speed = speed;
-		return obj;
-	},
-	update: function() {
-		
-	},
-	keyDown: function(event) {
 
-		if (event.keyCode == 65) {
-			console.log("herp!");
-		}
+	},
+
+var keysDown = {};
+
+	addEventListener("keydown", function (e) {
+		keysDown[e.keyCode] = true;
+	}, false);
+
+	addEventListener("keyup", function (e) {
+		delete keysDown[e.keyCode];
+	}, false);
+	
+		update: function() {
+	if (38 in keysDown) { // Player holding up
+		player.y -= player.speed;
+	}
+	if (40 in keysDown) { // Player holding down
+		hero.y += player.speed;
+	}
+	if (37 in keysDown) { // Player holding left
+		player.x -= player.speed;
+	}
+	if (39 in keysDown) { // Player holding right
+		player.x += player.speed;
 	}
 };
